cf10df812e07748913de54bcc2b96823dd2afe5c
Felix Exner <exner@fzi.de>
2024-10-20 20:01:12 +0200

    Add repository helper for adapting a workspace's src space

--- src/robot_folders/helpers/repository_helpers.py
+++ src/robot_folders/helpers/repository_helpers.py
@@ -25,12 +25,22 @@ This module contains helper functions around managing git repositories
 import os
 import subprocess
 import tempfile
-import yaml
 
 import click
 import git
+import yaml
+from vcstool import clients
+from vcstool.clients.vcs_base import VcsClientBase
+from vcstool.crawler import find_repositories
 
+import robot_folders.helpers.directory_helpers as dir_helpers
 from robot_folders.helpers.exceptions import ModuleException
+from robot_folders.workspaces import workspace
+
+
+class VCSCommand:
+    def __init__(self):
+        self.exact = False
 
 
 def parse_repository(repo_path, use_commit_id):
@@ -131,3 +141,169 @@ def clone_packages_from_dict(
     with open(repos_filename, "w") as repos_file:
         yaml.dump(repos, repos_file)
     clone_packages_from_repos_file(repos_filename, target_dir, clone_submodules)
+
+
+def parse_repo(repo: VcsClientBase, basename: str, exact: bool) -> tuple[str, dict]:
+    command = VCSCommand()
+    command.exact = exact
+
+    parsed = {}
+    rel_path = os.path.relpath(path=repo.path, start=basename)
+    parsed["type"] = repo.__class__.type
+    export_data = repo.export(command=command)["export_data"]
+    parsed["url"] = export_data["url"]
+    parsed["version"] = export_data["version"]
+    return (rel_path, parsed)
+
+
+def find_repos_in_path(
+    target_path: str, nested: bool = True, exact: bool = False
+) -> dict:
+    repos_found = find_repositories([target_path], nested=nested)
+
+    ret = {}
+    for repo in repos_found:
+        entry = parse_repo(repo, target_path, exact)
+        ret[entry[0]] = entry[1]
+
+    return ret
+
+
+def adapt_repos_from_dict(
+    repos_override: dict,
+    packages_dir: str,
+    clone_submodules: bool = True,
+    local_override_policy: str = "ask",
+    local_delete_policy: str = "ask",
+):
+    """
+    Parses the given repos_overwrite and compares it to the packages in repos.
+    """
+    repos = find_repos_in_path(packages_dir)
+    for repo_name in repos_override["repositories"].keys():
+        print(f"checking '{repo_name}'")
+        repo = repos_override["repositories"][repo_name]
+        local_version_exists = False
+        version_update_required = True
+        url_update_required = True
+        url = ""
+        version = ""
+
+        package_dir = os.path.join(packages_dir, repo_name)
+
+        if repo["type"] != "git":
+            click.echo(
+                f"Repo {repo_name} has type {repo['type']}. Currently, only the type 'git' is supported. Skipping package."
+            )
+            continue
+
+        if "version" in repo:
+            version = repo["version"]
+        else:
+            click.echo(
+                "WARNING: No version tag given for package '{}'. "
+                "The local version will be kept or the default branch "
+                "will be checked out for new package".format(repo_name)
+            )
+
+        if "url" in repo:
+            url = repo["url"]
+        else:
+            click.echo(
+                "WARNING: No url given for package '{}'. "
+                "Skipping package".format(repo_name)
+            )
+            continue
+
+        # compare the repos' versions and uris
+        if repo_name in repos.keys():
+            local_version_exists = True
+            local_repo = repos[repo_name]
+            print(local_repo)
+            version_update_required = False
+            if version != "" and version != local_repo["version"]:
+                click.echo(
+                    f"Package '{repo_name}' version '{version}' differs from local version '{local_repo['version']}'. "
+                )
+
+                if local_override_policy == "keep_local":
+                    click.echo(
+                        f"Going to keep the local version '{local_repo['version']}'"
+                    )
+                    version_update_required = False
+                elif local_override_policy == "override":
+                    click.echo(f"Going to use the updated version '{version}'")
+                    version_update_required = True
+                else:
+                    click.echo("Which version should be used?")
+                    click.echo("1) local version: {}".format(local_repo["version"]))
+                    click.echo("2) config_file version: {}".format(version))
+                    version_to_keep = click.prompt(
+                        "Which version should be used?",
+                        type=click.Choice(["1", "2"]),
+                        default="1",
+                    )
+                    version_update_required = version_to_keep == "2"
+
+            if url == local_repo["url"]:
+                url_update_required = False
+            elif local_override_policy == "keep_local":
+                url_update_required = False
+            elif local_override_policy == "override":
+                url_update_required = True
+            else:
+                click.echo(
+                    "Package '{}' url differs from local version. ".format(repo_name)
+                )
+                click.echo("local version: {}".format(local_repo["url"]))
+                click.echo("config_file version: {}".format(url))
+                version_to_keep = click.prompt(
+                    "Which url should be used?",
+                    type=click.Choice(["1", "2"]),
+                    default="2",
+                )
+                url_update_required = version_to_keep == "2"
+
+        else:
+            click.echo(
+                "Package '{}' does not exist in local structure. "
+                "Going to download.".format(repo_name)
+            )
+
+        # Create repo if it does not exist yet.
+        if not local_version_exists:
+            if clone_submodules:
+                subprocess.check_call(
+                    ["git", "clone", url, package_dir, "--recurse-submodules"]
+                )
+            else:
+                subprocess.check_call(["git", "clone", url, package_dir])
+
+        # Change the origin to the url specified
+        if url_update_required:
+            subprocess.check_call(
+                ["git", "remote", "set-url", "origin", url], cwd=package_dir
+            )
+
+        # Checkout the version specified
+        if version_update_required:
+            subprocess.check_call(["git", "fetch"], cwd=package_dir)
+            subprocess.check_call(["git", "checkout", version], cwd=package_dir)
+
+    for local_name in repos.keys():
+        if local_name not in repos_override["repositories"].keys():
+            click.echo(
+                "Package '{}' found locally, but not in target config.".format(
+                    local_name
+                )
+            )
+            package_dir = os.path.join(packages_dir, local_name)
+            if local_delete_policy == "delete_all":
+                dir_helpers.recursive_rmdir(package_dir)
+                click.echo("Deleted '{}'".format(local_name))
+            elif local_delete_policy == "ask":
+                if click.confirm("Do you want to delete it?"):
+                    dir_helpers.recursive_rmdir(package_dir)
+                    click.echo("Deleted '{}'".format(local_name))
+            elif local_delete_policy == "keep_all":
+                click.echo("Keeping repository as all should be kept")
--- tests/test_repository_helpers.py
+++ tests/test_repository_helpers.py
@@ -20,7 +20,9 @@
 # THE SOFTWARE.
 #
 
+import copy
 import os
+import shutil
 import tempfile
 
 import git
@@ -29,7 +31,7 @@ import yaml
 import robot_folders.helpers.repository_helpers as repo_helpers
 
 
-def create_git_repo(repo_path):
+def create_git_repo(repo_path: str) -> git.Repo:
     repo = git.Repo.init(repo_path, initial_branch="main")
 
     update_file = "simple_file.txt"
@@ -38,6 +40,8 @@ def create_git_repo(repo_path):
     add_file = [update_file]
     repo.index.add(add_file)
     repo.index.commit("Initial commit")
+
+    return repo
 
 
 def test_clone_repos_from_dict():
@@ -78,3 +82,131 @@ def test_clone_repos_from_file():
 
     assert os.path.exists(os.path.join(checkout_dir, "foo"))
     assert os.path.exists(os.path.join(checkout_dir, "foo", "simple_file.txt"))
+
+
+def test_find_repos():
+    repos_storage = tempfile.mkdtemp()
+    foo_repo_path = os.path.join(repos_storage, "foo")
+    create_git_repo(foo_repo_path)
+    bar_repo_path = os.path.join(repos_storage, "bar")
+    create_git_repo(bar_repo_path)
+
+    # Clone repos here:
+    ws_src = tempfile.mkdtemp()
+    git.Repo.clone_from(foo_repo_path, os.path.join(ws_src, "foo"))
+    git.Repo.clone_from(bar_repo_path, os.path.join(ws_src, "bar"))
+
+    repos_parsed = repo_helpers.find_repos_in_path(ws_src)
+
+    for repo_name in repos_parsed.keys():
+        repo = repos_parsed[repo_name]
+        assert repo["url"] == os.path.join(repos_storage, repo_name)
+
+
+def test_adapt_additional_repo():
+    # Create repo storage
+    # repos_storage
+    #   ├── foo
+    #   ├── bar
+    #   ├── bar_fork
+    repos_storage = tempfile.mkdtemp()
+    foo_repo_path = os.path.join(repos_storage, "foo")
+    foo_repo = create_git_repo(foo_repo_path)
+    bar_repo_path = os.path.join(repos_storage, "bar")
+    bar_repo = create_git_repo(bar_repo_path)
+    bar_repo.create_head("develop")
+    bar_fork_path = os.path.join(repos_storage, "bar_fork")
+    shutil.copytree(bar_repo_path, bar_fork_path)
+    bar_fork = git.Repo(bar_fork_path)
+
+    initial_data = {
+        "repositories": {
+            "foo": {"type": "git", "url": foo_repo_path, "version": "main"}
+        }
+    }
+    additional_data = {
+        "repositories": {
+            "bar": {"type": "git", "url": bar_repo_path, "version": "main"}
+        }
+    }
+    data_combined = copy.deepcopy(initial_data)
+    data_combined["repositories"]["bar"] = copy.deepcopy(
+        additional_data["repositories"]["bar"]
+    )
+
+    # Create target workspace with foo and bar
+    ws_src = tempfile.mkdtemp()
+    git.Repo.clone_from(foo_repo_path, os.path.join(ws_src, "foo"))
+
+    # Adapt ws_src containing initial_data to contain all projects from data_combined
+    repo_helpers.adapt_repos_from_dict(
+        repos_override=data_combined,
+        packages_dir=ws_src,
+        clone_submodules=True,
+        local_override_policy="keep_local",
+        local_delete_policy="keep_all",
+    )
+
+    assert os.path.exists(os.path.join(ws_src, "bar"))
+
+    bar_cloned = git.Repo(path=os.path.join(ws_src, "bar"))
+    assert str(bar_cloned.active_branch) == "main"
+    remote_branch = bar_cloned.active_branch.tracking_branch()
+    assert remote_branch
+    assert bar_cloned.remote(remote_branch.remote_name).url == bar_repo_path
+
+    print(additional_data)
+    # Modify repo to adapt
+    additional_data["repositories"]["bar"]["url"] = bar_fork_path
+    additional_data["repositories"]["bar"]["version"] = "develop"
+
+    # Adapt with different branch, but keep local versions
+    repo_helpers.adapt_repos_from_dict(
+        repos_override=additional_data,
+        packages_dir=ws_src,
+        clone_submodules=True,
+        local_override_policy="keep_local",
+        local_delete_policy="keep_all",
+    )
+    # Should still be origin with default branch since we used the `keep_Local` override policy
+    assert str(bar_cloned.active_branch) == "main"
+    remote_branch = bar_cloned.active_branch.tracking_branch()
+    assert remote_branch
+    assert bar_cloned.remote(remote_branch.remote_name).url == bar_repo_path
+
+    # Same as above but with `override` policy
+    repo_helpers.adapt_repos_from_dict(
+        repos_override=additional_data,
+        packages_dir=ws_src,
+        clone_submodules=True,
+        local_override_policy="override",
+        local_delete_policy="keep_all",
+    )
+    assert str(bar_cloned.active_branch) == "develop"
+    remote_branch = bar_cloned.active_branch.tracking_branch()
+    assert remote_branch
+    assert bar_cloned.remote(remote_branch.remote_name).url == bar_fork_path
+
+    print("test in question")
+    # Remove foo_repo from the adapted workspace, but do not delete local repos
+    repo_helpers.adapt_repos_from_dict(
+        repos_override=additional_data,
+        packages_dir=ws_src,
+        clone_submodules=True,
+        local_override_policy="keep_local",
+        local_delete_policy="keep_all",
+    )
+
+    assert os.path.exists(os.path.join(ws_src, "foo"))
+    assert os.path.exists(os.path.join(ws_src, "bar"))
+
+    # Remove foo_repo from the adapted workspace and delete local repos
+    repo_helpers.adapt_repos_from_dict(
+        repos_override=additional_data,
+        packages_dir=ws_src,
+        clone_submodules=True,
+        local_override_policy="keep_local",
+        local_delete_policy="delete_all",
+    )
+    assert not os.path.exists(os.path.join(ws_src, "foo"))
+    assert os.path.exists(os.path.join(ws_src, "bar"))
